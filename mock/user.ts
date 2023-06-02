4486634317511f7149f71309a9db96dc822ae542
feifei128 <545060094@qq.com>
2023-06-02 10:31:20 +0800

    mock

--- mock/user.ts
+++ mock/user.ts
@@ -44,10 +44,10 @@ export default {
     res.send({
       success: true,
       data: {
-        name: 'Serati Ma',
+        name: 'fifi',
         avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
         userid: '00000001',
-        email: 'antdesign@alipay.com',
+        email: 'lififi@gmail.com',
         signature: '海纳百川，有容乃大',
         title: '交互专家',
         group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
